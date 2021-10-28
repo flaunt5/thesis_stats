d4c9568de0b05207d5f0fdd8722fa3dc43898762
Joris Brandt <jorisbrandt@protonmail.com>
2021-10-29 01:53:03 +0200

    all stats implemented NaN-0 conversion

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 from tools import data
-import stats as st
+from stats import cronbach_alpha as ca
 import scipy.stats as sc
 import pandas as pd
 import numpy as np
@@ -8,21 +8,14 @@ import numpy as np
 
 # returns a Fisher-Z transform, which is equivalent to the Inverse hyperbolic tangent according to https://stats.stackexchange.com/questions/109028/fishers-z-transform-in-python
 def fisher_z(input):
-    print(input)
-    exit()
-    num = input + 0j
-    print(num)
-    return np.arctanh(num, 1)
+    if (input > 1.0):
+        num = input + 0j
+        return np.arctanh(num)
+    return input
 
 FirstData = data.FirstData("first_results/results.csv")
 SecondData = data.SecondData(
     "second_results/GEW_resu2.csv", "second_results/PANAS_resu2.csv")
-
-# firstAssocGEW = FirstData.get_gew_assoc_data()[['EMAIL']]
-# secondAssocGEW = SecondData.get_gew_assoc_data()[['RecipientEmail1']]
-
-# print(firstAssocGEW)
-# print(secondAssocGEW)
 
 firstNumGEW = FirstData.get_gew_num_data()
 secondNumGEW = SecondData.get_gew_num_data()
@@ -32,9 +25,8 @@ firstNumGEW = firstNumGEW[firstNumGEW.index.isin(
 secondNumGEW = secondNumGEW[secondNumGEW.index.isin(
     firstNumGEW.index.values)]
 
-GEWcorr = firstNumGEW.corrwith(secondNumGEW)
+GEWcorr = firstNumGEW.corrwith(secondNumGEW).replace(np.nan, 0)
 GEWcorr = GEWcorr.apply(fisher_z)
-
 firstNumPANAS = FirstData.get_panas_num_data()
 secondNumPANAS = SecondData.get_panas_num_data()
 
@@ -44,30 +36,22 @@ secondNumPANAS = secondNumPANAS[secondNumPANAS.index.isin(
     firstNumPANAS.index.values)]
 
 
-PANAScorr = firstNumPANAS.corrwith(secondNumPANAS).replace(np.inf, 0).replace(np.nan, 0)
-print(PANAScorr.values)
+PANAScorr = secondNumPANAS.corrwith(firstNumPANAS).replace(np.nan, 0)
 PANAScorr = PANAScorr.apply(fisher_z)
-exit()
-PANASavg = PANAScorr.dropna().values
-GEWavg = GEWcorr.dropna().values
+
+PANASavg = np.tanh(PANAScorr.mean())
+GEWavg = np.tanh(GEWcorr.mean())
 
 print(PANASavg)
 print(GEWavg)
-exit;
 
-ttesst = sc.ttest_ind(PANAScorr, GEWcorr)
+ttestavg = sc.ttest_ind(PANASavg, GEWavg)
+ttest = sc.ttest_ind(PANAScorr, GEWcorr)
 
-
-
-# print(GEWcorr)
-# print(PANAScorr)
-
-# firstGEWvalues = firstNumGEW.to_numpy().flatten()
-# secondGEWvalues = secondNumGEW.to_numpy().flatten()
-# firstPANASvalues = firstNumPANAS.to_numpy().flatten()
-# secondPANASvalues = secondNumPANAS.to_numpy().flatten()
-# print(sc.pearsonr(firstGEWvalues, secondGEWvalues))
-# print(sc.pearsonr(firstPANASvalues, secondPANASvalues))
+print(ca.cronbach_alpha(firstNumGEW))
+print(ca.cronbach_alpha(secondNumGEW))
+print(ca.cronbach_alpha(firstNumPANAS))
+print(ca.cronbach_alpha(secondNumPANAS))
 
 # GEWsub = secondNumGEW.sub(firstNumGEW)
 # PANASsub = secondNumPANAS.sub(firstNumPANAS)
